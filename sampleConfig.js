c28a18e97d5df07a833dd2a6e066b18d5d441e15
Alvaro Soneiro <soneiro@un.org>
2020-04-20 20:48:04 -0400

    First version

---
+++ sampleConfig.js
@@ -0,0 +1,16 @@
+var config = {
+        database: {
+            host: 'ip',
+            user: 'user',
+            password: 'password',
+            database : 'dbName'
+        },
+        certs: {
+            privKey: 'path2privkey/privkey.pem',
+            cert: 'path2cert/cert.pem'
+        }
+};
+
+module.exports = config;
+
+		
