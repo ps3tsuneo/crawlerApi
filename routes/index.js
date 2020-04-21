883631acbb553253af3759b104811b80dbc5cf9e
Alvaro Soneiro <soneiro@un.org>
2020-04-20 22:11:19 -0400

    Initial JWT implementation. Uses pre-shared key

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
+const config = require('../config');
+const { user } = config 
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/user/login', (req, res, next) => {
+  const { body } = req;
+  const { username } = body;
+  const { password } = body;
+
+
+  //checking to make sure the user entered the correct username/password combo
+  if(username === user.username && password === user.password) { 
+       console.log("Clownapggedon!!")
+      //if user log in success, generate a JWT token for the user with a secret key
+      jwt.sign({user}, 'privatekey', { expiresIn: '1h' },(err, token) => {
+          if(err) { console.log(err) }    
+          res.send(token);
+      });
+  } else {
+      console.log('ERROR: Could not log in');
+  }
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const jwt = require('jsonwebtoken');
 var router = express.Router();
 
 /* GET users listing. */
@@ -15,17 +16,45 @@ router.get('/all', function(req, res, next) {
   	});
 });
 
-router.get('/certs', function(req, res, next) {
-	console.log("Into the journey")
-	res.locals.connection.query('select * from certDomFqdnView', function (error, results, fields) {
-	  	if(error){
-	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
-	  		//If there is error, we send the error in the error section with 500 status
-	  	} else {
-  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  			//If there is no error, all is good and response is 200OK.
-	  	}
-  	});
+
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined') {
+
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+}
+
+
+router.get('/certs', checkToken, function(req, res, next) {
+	jwt.verify(req.token, 'privatekey', (err, authorizedData) => {
+		if(err){
+			//If error send Forbidden (403)
+			console.log('ERROR: Could not connect to the protected route');
+			res.sendStatus(403);
+		} else {
+			//If token is successfully verified, we can send the autorized data 
+			res.locals.connection.query('select * from certDomFqdnView', function (error, results, fields) {
+				if(error){
+					res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+					//If there is error, we send the error in the error section with 500 status
+				} else {
+					res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+					//If there is no error, all is good and response is 200OK.
+				}
+			});
+			console.log('SUCCESS: Connected to protected route');
+		}
+	})
 });
 
 module.exports = router;
